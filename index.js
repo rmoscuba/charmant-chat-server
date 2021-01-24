71ad37a83f095d68436805a6c68256892b51629f
rmoscuba <rmoscuba@gmail.com>
2021-01-24 17:03:18 -0500

    Basic socket.io chat server

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 const app = require('express')();
 const http = require('http').createServer(app);
+const io = require('socket.io')(http, {
+    cors: {
+      origin: "http://localhost:3000",
+      methods: ["GET", "POST"]
+    }
+  });
 
 const port = 4000
 
@@ -7,6 +13,13 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
 });
 
+io.on('connection', (socket) => {
+  socket.on('send', (message, user) => {
+    console.log(`message "${message} -> "${user}" `);
+    io.to(user).emit('message', { user: user, msg: message });
+    console.log(`message emited`);
+  });
+});
 
 http.listen(port, () => {
   console.log(`listening on *:${port}`);
